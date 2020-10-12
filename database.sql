9fe42cc029584cc17516c44361555bfcbae7d803
Emma Westbrook <emmalwestbrook@gmail.com>
2020-10-12 10:15:44 -0500

    database.sql updated with beginner sql code

--- database.sql
+++ database.sql
@@ -3,8 +3,50 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
+
+--Create a database called "letterpals"
+
+--Creating user table
+CREATE TABLE "user"
+(
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "name" VARCHAR ,
+    "about" VARCHAR,
+    "address" VARCHAR
 );
+-- DROP TABLE "user";
+
+--creating letter table
+CREATE TABLE "letter"
+(
+    "id" SERIAL PRIMARY KEY,
+    "from_id" INT REFERENCES "user",
+    "to_id" INT REFERENCES "user",
+    "postmark" DATE,
+    "recieved" BOOLEAN
+);
+-- DROP TABLE "letters";
+
+--creating pal table
+CREATE TABLE "pal"
+(
+    "id" SERIAL PRIMARY KEY,
+    "pal1_id" INT REFERENCES "user",
+    "pal2_id" INT REFERENCES "user"
+);
+-- DROP TABLE "pals";
+
+--AXIOS QUERIES
+--creating pal list for main app view
+SELECT "pal"."id" AS "friendship_id",
+    "pal1"."id" AS "pal1_id",
+    "pal2"."id"AS "pal2_id",
+    "pal1"."name" AS "pal1_username",
+    "pal2"."name" AS "pal2_username"
+
+FROM "pal" AS "pal"
+    JOIN "user" "pal1" ON "pal1"."id"="pal"."pal1_id"
+    JOIN "user" "pal2" ON "pal2"."id"="pal"."pal2_id"
+WHERE "pal1_id"=1 OR "pal2_id"=1;
