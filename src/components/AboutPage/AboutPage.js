77a4772a58d7e7011086bd3e092fdcd46c058d94
Emma Westbrook <emmalwestbrook@gmail.com>
2020-10-12 10:59:51 -0500

    playing with colors

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -8,7 +8,7 @@ import React from 'react';
 const AboutPage = () => (
   <div className="container">
     <div>
-      <p>This about page is for anyone to read!</p>
+      <p>Letterpals is a web app designed to help users keep track of their postal correspondance.</p>
     </div>
   </div>
 );
