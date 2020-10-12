831ceb780b4692f484f8feb5e58e83e393a5721e
Emma Westbrook <emmalwestbrook@gmail.com>
2020-10-12 11:14:40 -0500

    PalList template component made and linked in navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import PalList from '../PalList/PalList';
 
 import './App.css';
 
@@ -60,6 +61,13 @@ class App extends Component {
               exact
               path="/info"
               component={InfoPage}
+            />
+
+            <ProtectedRoute
+              // logged in shows PalList else shows LoginPage
+              exact
+              path="/pallist"
+              component={PalList}
             />
 
             {/* When a value is supplied for the authRedirect prop the user will
