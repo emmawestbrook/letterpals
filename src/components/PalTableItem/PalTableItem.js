6402f2e482d8d9fb05646c149d20489d34eb2e5f
Emma Westbrook <emmalwestbrook@gmail.com>
2020-10-12 19:27:00 -0500

    pals render on dom and button leads to pal details page

--- src/components/PalTableItem/PalTableItem.js
+++ src/components/PalTableItem/PalTableItem.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import { withRouter } from 'react-router-dom';
+
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
@@ -8,8 +10,23 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 // component.
 class PalTableItem extends Component {
     state = {
-        heading: 'Pal Table Item',
+        pal_id: 
+        this.props.userId === this.props.pal.pal1_id ?
+        this.props.pal.pal2_id :
+        this.props.pal.pal1_id
     };
+
+    componentDidMount() {
+        console.log(this.state);
+    };
+
+    handleClick = () => {
+        this.props.dispatch({
+            type: 'FETCH_PAL', 
+            payload: this.state.pal_id
+        });
+        this.props.history.push(`/palprofile`);
+    }
 
     render() {
         return (
@@ -20,10 +37,10 @@ class PalTableItem extends Component {
                         this.props.pal.pal2_name :
                         this.props.pal.pal1_name
                 }</td>
-                <td><button>details</button> </td>
+                <td><button onClick={this.handleClick}>details</button> </td>
             </tr>
         );
     }
 }
 
-export default connect(mapStoreToProps)(PalTableItem);
+export default connect(mapStoreToProps)(withRouter(PalTableItem));
