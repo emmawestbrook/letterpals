4adef9f6f1621402382dc067f65f63ec0ed24306
Emma Westbrook <emmalwestbrook@gmail.com>
2020-10-14 15:29:33 -0500

    added multer

--- src/components/EditMyProfile/EditMyProfile.js
+++ src/components/EditMyProfile/EditMyProfile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import multer from 'multer';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
