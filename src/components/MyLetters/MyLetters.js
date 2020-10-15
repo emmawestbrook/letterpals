70b57a1808219935d08e838a6ccaf085ea8b6a32
Emma Westbrook <emmalwestbrook@gmail.com>
2020-10-15 12:35:41 -0500

    last commit before lunch

--- src/components/MyLetters/MyLetters.js
+++ src/components/MyLetters/MyLetters.js
@@ -8,17 +8,55 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 // component.
 class MyLetters extends Component {
     state = {
-        heading: 'My Letters',
+        fromMe: [],
+        toMe: []
     };
 
     componentDidMount() {
-        this.props.dispatch({ type: 'GET_LETTERS' });
+        this.props.dispatch({
+            type: 'GET_LETTERS'
+        });
+
+        //this.letterSort(this.props.store.letters);
+
+        // this.setState({
+        //     fromMe: letters.fromMe,
+        //     toMe: letters.toMe
+        // });
+
     };
 
+    // letterSort = (letters) => {
+    //     let lettersFromMe = [];
+    //     let lettersToMe = [];
+    //     console.log('letters', letters);
+    //     for (let i = 0; i < letters.length; i++) {
+    //         if (letters[i].from_id === this.props.store.user.id) {
+    //             lettersFromMe.push(letters[i]);
+    //         }
+    //         else if (letters[i].from_id === this.props.store.user.id) {
+    //             lettersToMe.push(letters[i]);
+    //         }
+    //     }
+    //     let myLetters = {
+    //         fromMe: lettersFromMe,
+    //         toMe: lettersToMe
+    //     };
+    //     console.log('my letters', myLetters);
+    //     this.setState({
+    //         fromMe: letters.fromMe,
+    //         toMe: letters.toMe
+    //     });
+    //     return myLetters;
+    // }
+
     render() {
+
+
         return (
             <div>
                 {JSON.stringify(this.props.store.letters)}
+
             </div>
         );
     }
