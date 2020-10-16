95d979adca150415db68127d5aca98d69f37c446
Emma Westbrook <emmalwestbrook@gmail.com>
2020-10-16 10:14:27 -0500

    added material-ui lab for autocomplete

--- src/components/PalSearch/PalSearch.js
+++ src/components/PalSearch/PalSearch.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
@@ -14,7 +16,14 @@ class PalSearch extends Component {
     render() {
         return (
             <div>
-                <h2>{this.state.heading}</h2>
+                <h2>{this.state.heading}</h2>'
+                <Autocomplete
+                    id="combo-box-demo"
+                    options={this.props.store.pals}
+                    getOptionLabel={(option) => option.name}
+                    style={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+                />
             </div>
         );
     }
