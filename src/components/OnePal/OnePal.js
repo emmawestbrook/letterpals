a72da429f15540a41f732458e982fdd9d215fadb
Emma Westbrook <emmalwestbrook@gmail.com>
2020-10-19 17:48:33 -0500

    somehow i broke adding a letter

--- src/components/OnePal/OnePal.js
+++ src/components/OnePal/OnePal.js
@@ -17,9 +17,7 @@ class TemplateClass extends Component {
         this.setState({ addPal: true });
         console.log(this.state);
         console.log('ONEUSER.ID', this.props.store.oneuser);
-        // for (let i = 0; i < this.props.store.pal.length; i++) {
-        //     if (this.props.store.pal.pal1_id ===)
-        // }
+
         this.props.dispatch({
             type: 'ADD_PAL',
             payload: this.props.store.oneuser
@@ -29,14 +27,18 @@ class TemplateClass extends Component {
     render() {
         return (
             <div className="palReturn">
-                <h1>{this.props.store.oneuser.name}</h1>
-                <img src={this.props.store.oneuser.avatar} width="200" alt="avatar" className="profileimg" />
-                <h2 className="username">@{this.props.store.oneuser.username}</h2>
-                <p>{this.props.store.oneuser.about}</p>
-                {this.state.addPal ?
-                    <p>pal added!</p> :
-                    <button className="btn" onClick={this.handleClick}>add pal</button>
-                }
+                <div className="palpic">
+                    <img src={this.props.store.oneuser.avatar} width="200" alt="avatar" className="profileimg" />
+                    <h2 className="username">@ {this.props.store.oneuser.username}</h2>
+                    {this.state.addPal ?
+                        <p>pal added!</p> :
+                        <button className="btn" onClick={this.handleClick}>add pal</button>
+                    }
+                </div>
+                <div className="palinfo">
+                    <h1>{this.props.store.oneuser.name}</h1>
+                    <p>{this.props.store.oneuser.about}</p>
+                </div>
             </div>
         );
     }
