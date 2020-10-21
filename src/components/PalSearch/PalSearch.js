5e7a85357d4ddb1062ed9a3565c6b82ebf0603e5
Emma Westbrook <emmalwestbrook@gmail.com>
2020-10-21 14:03:38 -0500

    this is a mess

--- src/components/PalSearch/PalSearch.js
+++ src/components/PalSearch/PalSearch.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import TextField from '@material-ui/core/TextField';
+import Autocomplete from 'react-autocomplete';
+// import Autocomplete from '@material-ui/lab/Autocomplete';
+// import TextField from '@material-ui/core/TextField';
 import OnePal from '../OnePal/OnePal';
 import './PalSearch.css';
+import { colors } from '@material-ui/core';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
@@ -48,30 +50,49 @@ class PalSearch extends Component {
         }
         console.log(this.state);
     }
+
     render() {
         return (
             <div>
                 <h1 className="title">pal search</h1>
                 <div className="searchDiv">
-                    <Autocomplete
-                        freeSolo
-                        id="pal-search"
-                        options={this.props.store.allusers}
-                        getOptionLabel={(option) => option.username}
-                        //value={this.props.store.allusers.id}
-                        onChange={this.handleChange}
-                        //onInputChange={(inputValue) => this.handleInputChange(inputValue)}
 
-                        style={{ width: 600 }}
-                        renderInput={(params) => (
-                            <TextField
-                                {...params}
-                                label="Search for username"
-                                margin="normal"
-                                variant="outlined"
-                                InputProps={{ ...params.InputProps, type: 'search' }}
-                            />)}
+                    <Autocomplete
+                        getItemValue={(item) => item.id}
+                        items={this.props.store.allusers}
+                        renderItem={(item, isHighlighted) =>
+                            <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                                {item.username}
+                            </div>
+                        }
+                        value={(item) => item.id}
+                        //onChange={(e) => value = e.target.value}
+                        onSelect={(val) => this.handleChange(val)}
+                    //onChange={(event, value) => this.handleChange(event, value)}
+                    //onInputChange={(inputValue) => this.handleInputChange(inputValue)}
+                    // value={this.props.store.allusers.id}
+                    // onSelect={(e) => this.handleChange(e,)}
+                    //onSelect={value => value = val}
+
+
+                    // id="pal-search"
+                    // options={this.props.store.allusers}
+                    // getOptionLabel={(option) => option.username}
+                    // //value={this.props.store.allusers.id}
+                    // onChange={this.handleChange}
+                    // //onInputChange={(inputValue) => this.handleInputChange(inputValue)}
+                    // style={{ borderColor: 'orange', width: 600 }}
+                    // renderInput={(params) => (
+                    //     <TextField
+                    //         {...params}
+                    //         label="Search for username"
+                    //         margin="normal"
+                    //         variant="outlined"
+                    //         InputProps={{ ...params.InputProps, type: 'search' }}
+                    //     />)}
                     />
+
+
                     <button className="btn" onClick={this.handleSearch}>search</button>
                 </div>
                 {this.state.renderPal &&
